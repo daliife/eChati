cfc4aa6eac74adb6e757d9349c502fabf9273233
daliife <compresdavid@gmail.com>
2014-05-11 19:40:08 +0200

    commit 2
    Grans canvis!!!

--- echatiC.c
+++ echatiC.c
@@ -1,54 +1,130 @@
-	opcio=0;
+%--------------------------------------------------------------------------
+%
+%           ECHATI, el buscador de parelles ideal!
+%
+%   by Pablo Gonzalez, Dani Reixats, Carlos Quintana i David Gimeno
+%--------------------------------------------------------------------------
 
-	//carreguem dades de l'arxiu proporcionat
-	load ('eChati.h');
+%INICI DEL PROGRAMA
 
-	prompt = 'BENVINGUT A eCHATI!\n1.Mostrar un usuari de la base de dades i la seva imatge reconstruida.\n2.\nSelecciona una opcio (1-5): ';
-	result = input(prompt);
+%Carreguem l'axiu amb totes les dades a tractar de eChati
+load eChati.mat
+opcio = 1;
+
+%Descomposicio en valors singulars per facilitar despres el calcul       
+[U,S,V] = svd(double(faces_database));
+Utinici = U(:,1:30);
+Stinici = S(1:30,1:30);
+Vtinici = V(:,1:30);
+Vtranspinici = transpose(Vtinici);
+Arecinici = Utinici * Stinici * Vtranspinici;
 
 
-	while(opcio!=5){
+while opcio~=5
+    
+    %Demanem a l'usuari que entri una opcio de les disponibles
+    prompt = '\nBENVINGUT A ECHATI\n\n1.Mostrar un usuari de la base de dades i la seva imatge reconstruida.\n2.Mostrar els usuaris que busquen parella.\n3.eChati estandard.\n4.eChati Premium.\n5.Sortir del programa.\n\nSelecciona una opcio (1-5): ';
+    opcio = input(prompt);
 
-		if (!isempty(opcio)){
-			switch(opcio){
-				case 1:
-					prompt = '\nEscriu el numero de usuari que vols mostrar';
-					num_usuari = input(prompt);
-					original = reshape(faces_database(:,num_usuari),100,90);
-					
 
-					prompt = '\nEscriu el numero de components que vols que tingui la imatge reconstruida';
-					p = input(prompt);
+    switch opcio
 
-					//Descomposicio valors singulars
-					[U,S,V] = svd(double(faces_database));
-					Ut = U(:,1:p);
-					St = S(1:p,1:p);
-					Vt = V(:,1:p);
+        case 1
+            %Demanem a l'usuari quin usuari de la base de dades vol mostrar
+            prompt = '\nHAS SELECCIONAT LA OPCIO 1:\nEscriu el numero dusuari a mostrar (1-38): ';
+            num_usuari_base= input(prompt);
 
-					//Transposem la matriu V
-					Vtransp = transpose(Vt);
+            %Recreem de la matriu faces_database el usuari corresponent en una
+            %nova matriu de 100 per 90
+            original = reshape(faces_database(:,num_usuari_base),100,90);
 
-					//Formem la nova matriu truncada
-					Arec = Ut * St * Vtransp;
+            %Demanem a l'usuari el nombre de components que vol agafar per fer
+            %la descomposicio en valors singulars
+            prompt = 'Escriu el numero de components que vols agafar per fer la descomposicio: '; 
+            p = input(prompt);
 
-					//Redimensionem i mostrem per pantalla l'usuari corresponent.
-					reconstruida = reshape(Arec(:,num_usuari), 100, 90);
-					subplot(1,2,1), imshow(mat2gray(original));
-					subplot(1,2,2), imshow(mat2gray(reconstruida));
+            %Descomposicio en valors singulars
+            [U,S,V] = svd(double(faces_database));
+            Ut = U(:,1:p);
+            St = S(1:p,1:p);
+            Vt = V(:,1:p);
+            Vtransp = transpose(Vt);
+            Arec = Ut * St * Vtransp;
 
-				case 2:
+            %Recreem la matriu recontruida/truncada per la dvs
+            reconstruida = reshape(Arec(:,num_usuari_base), 100, 90);
 
-				case 3:
+            %Mostrem les dues imatges, original i reconstruida, en una nova
+            %finestra
 
-				case 4:
+            figure('Name','RESULTAT OPCIO 1','NumberTitle','off');
+            subplot(1,2,1), imshow(mat2gray(original));
+            subplot(1,2,2), imshow(mat2gray(reconstruida));
 
-				case 5:
-			}
-		}else{
-			prompt= '\nERROR: Escriu una opcio correcta (1-5).\n';
-		}
+        case 2
 
-	}
-}
+            figure('Name','RESULTAT OPCIO 2','NumberTitle','off');
+            subplot(1,2,1), imshow(mat2gray(original));
+            subplot(1,2,2), imshow(mat2gray(reconstruida));
+            
+            
+        case 3
+            prompt = '\nHAS SELECCIONAT LA OPCIO 3:\nEscriu el numero dusuari que esta cercant parella (1-10): ';
+            num_usuari= input(prompt);
+
+            sexe = input('Escriu el sexe de la parella que busca aquest usuari (H/M= masculi // D/F=femeni): ','s');
+            
+            %Creem una nova variable per despres comprovar el genere al
+            %vector boolea gender_database
+            if sexe=='H' || sexe=='M' || sexe=='h' || sexe=='m'
+                num_sexe=1;
+            else
+                num_sexe=0;
+            end
+            
+            
+            %Comparem un a un el vector de l'usuari seleccionat i tots els
+            %usuaris del sexe seleccionat per trobar al mes semblant
+            for index_for = 1:19
+                if num_sexe == gender_database(index_for)
+                    
+                    
+                end
+            end
+            
+            %Resultat a
+            
+            
+        case 4
+            
+        case 5
+            opcio = 5;
+
+        otherwise
+             prompt = '\n\ERROR! Numero dopcio incorrecta (1-5)\nClicka ENTER per tornar al menu principal...'; 
+             enter = input(prompt);
+             opcio = 1;
+    end
+    
+    prompt = 'Clicka ENTER per tornar al menu principal...'; 
+    enter = input(prompt);
+
+        
+end
+
+
+prompt = '\nPrograma finalitzat amb èxit.\nPrem ENTER per acabar...'; 
+enter = input(prompt);
+
+
+
+
+
+
+
+
+
+
+
+
 
